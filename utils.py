a40a25cf6bf96078b19849f86c1b91c0eec79135
Mohamed Samir <72920742+Mo-Sam-Mo@users.noreply.github.com>
2023-05-04 04:59:14 -0700

    Update utils.py
    fix not found problem

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from torchvision import transforms
 from handlers import MNIST_Handler, SVHN_Handler, CIFAR10_Handler
-from data import get_MNIST, get_FashionMNIST, get_SVHN, get_CIFAR10
+from data import get_MNIST, get_UnbalancedMNIST, get_FashionMNIST, get_SVHN, get_CIFAR10
 from nets import Net, MNIST_Net, SVHN_Net, CIFAR10_Net
 from query_strategies import RandomSampling, LeastConfidence, MarginSampling, EntropySampling, \
                              LeastConfidenceDropout, MarginSamplingDropout, EntropySamplingDropout, \
